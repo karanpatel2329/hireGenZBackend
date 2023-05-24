4aa08700e9a71e8a0369660136c3e3e0d1c30e0e
roshan71 <patelroshan882@gmail.com>
2023-05-24 20:26:56 +0530

    change port no in sendemail

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -6,7 +6,7 @@ const sendEmail = async (email, subject, text) => {
     const transporter = nodemailer.createTransport({
       host: process.env.HOST,
       service: process.env.SERVICE,
-      port: 587,
+      port: 465,
       secure: true,
       auth: {
         user: process.env.USER,
