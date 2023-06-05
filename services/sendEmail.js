590302bab049904670b549ce32247e1400883a67
roshan71 <patelroshan882@gmail.com>
2023-06-05 12:53:45 +0530

    change Email Sending Server

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -1,33 +1,25 @@
 const nodemailer = require("nodemailer");
-const {google}=require("googleapis");
-const config=require("./config.js")
 const dotenv=require("dotenv");
 dotenv.config();
-const OAuth2=google.auth.OAuth2;
 
-
-const OAuth2_client=new OAuth2(config.clientId,config.clientSecret);
-OAuth2_client.setCredentials({refresh_token:config.refreshToken});
 
 const sendEmail = async (email, subject, text) => {
   try {
-    const accessToken=OAuth2_client.getAccessToken();
-    // console.log(accessToken);
     
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
+   const transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      service: process.env.SERVICE,
+      port: 465,
+      secure: true,
       auth: {
-      type:'OAuth2',
-      user:config.user,
-      clientId:config.clientId,
-      clientSecret:config.clientSecret,
-      refreshToken:config.refreshToken,
-      accessToken:accessToken
+        user: process.env.EMAILUSER,
+        pass: process.env.EMAILPASS,
       },
     });
 
+
     await transporter.sendMail({
-      from:`HireGenZ <${process.env.USER}>`,
+      from:`HireGenZ <${process.env.EMAILUSER}>`,
       to: email,
       subject: subject,
       text: text,
